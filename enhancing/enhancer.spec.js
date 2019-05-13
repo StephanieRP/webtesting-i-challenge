3e66634d566d5f28e3408abe01cdf832bc5bd8c2
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-05-13 17:05:17 -0500

    added more test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -8,6 +8,12 @@ let test = {
   durability: 75
 };
 
+let test2 = {
+  name: "Test 2",
+  enhancement: 20,
+  durability: 75
+};
+
 describe("enhancer.js", () => {
   //repair test
   describe("repair() function", () => {
@@ -15,6 +21,13 @@ describe("enhancer.js", () => {
       expect(enhancer.repair(test)).toStrictEqual({
         name: "Test",
         enhancement: 6,
+        durability: 100
+      });
+    });
+    it("repair item by returning durability as 100", () => {
+      expect(enhancer.repair(test2)).toStrictEqual({
+        name: "Test 2",
+        enhancement: 20,
         durability: 100
       });
     });
@@ -28,6 +41,13 @@ describe("enhancer.js", () => {
         durability: 75
       });
     });
+    it("enhancement increase by 1, if at 20 it will not change, durability stays the same", () => {
+      expect(enhancer.succeed(test2)).toStrictEqual({
+        name: "Test 2",
+        enhancement: 20,
+        durability: 75
+      });
+    });
   });
   //fail test
   describe("fail() function", () => {
@@ -36,6 +56,13 @@ describe("enhancer.js", () => {
         name: "Test",
         enhancement: 6,
         durability: 70
+      });
+    });
+    it("failure test", () => {
+      expect(enhancer.fail(test2)).toStrictEqual({
+        name: "Test 2",
+        enhancement: 19,
+        durability: 65
       });
     });
   });
