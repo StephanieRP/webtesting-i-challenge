72981d4607f496f50a908c20a2fff552ae99d4f7
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-05-13 16:49:15 -0500

    completed all test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,45 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
+// When enhancement succeeds
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
+
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement < 20 ? item.enhancement + 1 : item.enhancement
+  };
 }
 
+// When enhancement fails
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement:
+      item.enhancement > 16 ? item.enhancement - 1 : item.enhancement,
+    durability:
+      item.enhancement < 15
+        ? item.durability - 5
+        : item.enhancement >= 15
+        ? item.durability - 10
+        : item.durability
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,42 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+//item
+
+let test = {
+  name: "Test",
+  enhancement: 6,
+  durability: 75
+};
+
+describe("enhancer.js", () => {
+  //repair test
+  describe("repair() function", () => {
+    it("repair item by returning durability as 100", () => {
+      expect(enhancer.repair(test)).toStrictEqual({
+        name: "Test",
+        enhancement: 6,
+        durability: 100
+      });
+    });
+  });
+  //success test
+  describe("success() function", () => {
+    it("enhancement increase by 1, if at 20 it will not change, durability stays the same", () => {
+      expect(enhancer.succeed(test)).toStrictEqual({
+        name: "Test",
+        enhancement: 7,
+        durability: 75
+      });
+    });
+  });
+  //fail test
+  describe("fail() function", () => {
+    it("failure test", () => {
+      expect(enhancer.fail(test)).toStrictEqual({
+        name: "Test",
+        enhancement: 6,
+        durability: 70
+      });
+    });
+  });
+});
